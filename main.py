d583d704c4a91041984625550facb53baa7abcd7
halsing <adrian.zieba2@gmail.com>
2020-01-08 18:36:53 +0100

    first commit of Thanos_py

---
+++ main.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python3
+
+import os
+import time
+import tkinter as tk
+from tkinter import ttk, filedialog
+
+from thanos import ThanosGlove
+
+
+class ThanosMind(tk.Tk):
+
+    length_del_files = 0
+    all_files = []
+    error_files = []
+    directory = ''
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side='top', fill='both', expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        
+        for F in (StartPage,DirnamePage, LoadingPage, FinalPage):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="NESW")
+
+        self.show_frame(StartPage)
+
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controler):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="StartPage")
+        label.pack()
+        start_button = ttk.Button(
+            self, 
+            text="Let's start!", 
+            command=lambda: controler.show_frame(DirnamePage))
+        start_button.pack()
+
+        exit_button = ttk.Button(
+            self, 
+            text="Exit", 
+            command=controler.destroy)
+        exit_button.pack()
+
+
+class DirnamePage(tk.Frame):
+    def __init__(self, parent, controler):
+        tk.Frame.__init__(self, parent)
+        self.controler = controler
+        self.parent = parent
+        
+        label = tk.Label(self, text="Where do You want use Thanos ?")
+        label.pack()
+
+        dir_location = tk.Entry(self, width=100)
+        dir_location.pack()
+        dir_location.delete(0, tk.END)
+        dir_location.insert(0, str(os.getcwd()))
+
+        confirm_button = ttk.Button(
+            self, 
+            text="Confirm", 
+            command=lambda: self.thanos_confirm(dir_location.get()))
+        confirm_button.pack()
+
+        exit_button = ttk.Button(
+            self, 
+            text="Exit", 
+            command=controler.destroy)
+        exit_button.pack()
+        
+    def thanos_confirm(self, location: str) -> None:
+        correct_dirname = ThanosGlove.check_dirname(str(location))
+        if correct_dirname is True:
+            self.controler.directory = str(location)
+            self.controler.show_frame(LoadingPage)
+        else:
+            pass
+
+
+class LoadingPage(tk.Frame):
+    def __init__(self, parent, controler):
+        tk.Frame.__init__(self, parent)
+        self.controler = controler
+        label = tk.Label(self, text="Loading Page")
+        label.pack()
+
+        load_button = ttk.Button(
+            self, 
+            text="Are You sure to do this ?", 
+            command=lambda: self.check_directory())
+        load_button.pack()
+
+        exit_button = ttk.Button(
+            self, 
+            text="Exit", 
+            command=controler.destroy)
+        exit_button.pack()
+
+    def check_directory(self):
+        all_files = ThanosGlove.list_of_files(self.controler.directory)
+        del_list_length = ThanosGlove.remove_files(all_files)
+        self.controler.length_del_files = del_list_length
+
+
+class FinalPage(tk.Frame):
+    def __init__(self, parent, controler):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="StartPage")
+        label.pack()
+        
+        exit_button = ttk.Button(
+            self, 
+            text="Exit", 
+            command=controler.destroy)
+        exit_button.pack()
+
+
+if __name__ == "__main__":
+    app = ThanosMind()
+    app.title("Thanos.py")
+    app.geometry("280x100")
+    app.mainloop()
---
+++ thanos.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""
+Module contain thanos.py main functionality like:
+    - checking dirnames
+    - walk through directries for collecting all files
+    - remove randomly half of the files
+"""
+
+import os
+import random
+import sys
+import time
+
+from typing import List
+
+ListStrType = List[str]
+
+
+class ThanosGlove:
+    """
+    Class contains functionality of Thanos.py
+    """
+
+    @staticmethod
+    def check_dirname(dir_path: str) -> bool:
+        """
+        Method check if path is path of directory, not a file
+        """
+        return os.path.isdir(dir_path)
+
+    @staticmethod
+    def list_of_files(dirname: str) -> ListStrType:
+        """
+        Return list of files in directory and every subdirectories
+        """
+        filenames = []
+
+        for root, dirs, files in os.walk(dirname):
+            for current_file in files:
+                full_path = os.path.join(root, current_file)
+                filenames.append(full_path)
+
+        return filenames
+
+    @staticmethod
+    def remove_files(list_of_files: ListStrType) -> int:
+        """Function remove random pernamently files from the list_of_files"""
+
+        list_length = len(list_of_files)
+
+        for n_time in range(round(list_length/2)):
+            del_file = random.choice(list_of_files)
+            os.remove(del_file)
+            list_of_files.remove(del_file)
+        return len(list_of_files)
